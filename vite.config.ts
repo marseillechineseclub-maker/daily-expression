19d89c837971bcc6393d3c9fd10cfc89d7af53e7
marseillechineseclub-maker <marseillechineseclub@gmail.com>
2026-02-03 17:04:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-base: '/daily-expression/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/daily-expression/',
   plugins: [react(), tailwindcss()],
 })
